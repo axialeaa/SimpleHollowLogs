8afa1704a899b046dc993ac487690649acf6b865
Axia <mcvoidredstone@gmail.com>
2023-10-09 17:51:33 +0100

    added icon

--- src/main/java/com/axialeaa/simplehollowlogs/MainEntrypoint.java
+++ src/main/java/com/axialeaa/simplehollowlogs/MainEntrypoint.java
@@ -3,13 +3,12 @@ package com.axialeaa.simplehollowlogs;
 import com.axialeaa.simplehollowlogs.block.ModBlocks;
 import com.axialeaa.simplehollowlogs.block.ModItemGroups;
 import com.axialeaa.simplehollowlogs.util.ModBlockBehaviours;
+import com.axialeaa.simplehollowlogs.world.HollowLogPlacedFeatures;
 import net.fabricmc.api.ModInitializer;
-
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.gen.GenerationStep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,9 +23,14 @@ public class MainEntrypoint implements ModInitializer {
 		ModItemGroups.registerItemGroups();
 		ModBlockBehaviours.setFlammability();
 
-		FabricLoader.getInstance().getModContainer(MOD_ID).ifPresent(modContainer ->
-				ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("axialeaa", "natural_gen"), modContainer, Text.of("Natural Generation"), ResourcePackActivationType.NORMAL)
-		);
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.BIRCH_FOREST, BiomeKeys.OLD_GROWTH_BIRCH_FOREST), GenerationStep.Feature.VEGETAL_DECORATION, HollowLogPlacedFeatures.FALLEN_BIRCH_FOREST_LOG);
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.CHERRY_GROVE), GenerationStep.Feature.VEGETAL_DECORATION, HollowLogPlacedFeatures.FALLEN_CHERRY_GROVE_LOG);
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.DARK_FOREST), GenerationStep.Feature.VEGETAL_DECORATION, HollowLogPlacedFeatures.FALLEN_DARK_FOREST_LOG);
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST, BiomeKeys.FLOWER_FOREST), GenerationStep.Feature.VEGETAL_DECORATION, HollowLogPlacedFeatures.FALLEN_FOREST_LOG);
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.JUNGLE, BiomeKeys.BAMBOO_JUNGLE), GenerationStep.Feature.VEGETAL_DECORATION, HollowLogPlacedFeatures.FALLEN_JUNGLE_LOG);
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.SPARSE_JUNGLE), GenerationStep.Feature.VEGETAL_DECORATION, HollowLogPlacedFeatures.FALLEN_SPARSE_JUNGLE_LOG);
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.TAIGA, BiomeKeys.SNOWY_TAIGA, BiomeKeys.GROVE, BiomeKeys.OLD_GROWTH_PINE_TAIGA, BiomeKeys.OLD_GROWTH_SPRUCE_TAIGA), GenerationStep.Feature.VEGETAL_DECORATION, HollowLogPlacedFeatures.FALLEN_TAIGA_LOG);
+		BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.WINDSWEPT_FOREST), GenerationStep.Feature.VEGETAL_DECORATION, HollowLogPlacedFeatures.FALLEN_WINDSWEPT_FOREST_LOG);
 
 		LOGGER.info(MOD_NAME + " initialized. That's all you need to know. This is a hollow logger.");
 	}
---
+++ src/main/java/com/axialeaa/simplehollowlogs/world/HollowLogPlacedFeatures.java
@@ -0,0 +1,23 @@
+package com.axialeaa.simplehollowlogs.world;
+
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.feature.PlacedFeature;
+
+public class HollowLogPlacedFeatures {
+
+    public static final RegistryKey<PlacedFeature> FALLEN_BIRCH_FOREST_LOG = registerKey("fallen_birch_forest_log");
+    public static final RegistryKey<PlacedFeature> FALLEN_CHERRY_GROVE_LOG = registerKey("fallen_cherry_grove_log");
+    public static final RegistryKey<PlacedFeature> FALLEN_DARK_FOREST_LOG = registerKey("fallen_dark_forest_log");
+    public static final RegistryKey<PlacedFeature> FALLEN_FOREST_LOG = registerKey("fallen_forest_log");
+    public static final RegistryKey<PlacedFeature> FALLEN_JUNGLE_LOG = registerKey("fallen_jungle_log");
+    public static final RegistryKey<PlacedFeature> FALLEN_SPARSE_JUNGLE_LOG = registerKey("fallen_sparse_jungle_log");
+    public static final RegistryKey<PlacedFeature> FALLEN_TAIGA_LOG = registerKey("fallen_taiga_log");
+    public static final RegistryKey<PlacedFeature> FALLEN_WINDSWEPT_FOREST_LOG = registerKey("fallen_windswept_forest_log");
+
+    public static RegistryKey<PlacedFeature> registerKey(String name) {
+        return RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier("axialeaa", name));
+    }
+
+}
