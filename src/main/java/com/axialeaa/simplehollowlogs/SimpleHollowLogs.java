c0d2b3abc76f846eb0e5678a048040df1acf9a1b
Axia <mcvoidredstone@gmail.com>
2023-10-13 22:33:49 +0100

    moved feature placement into the behaviours class

--- src/main/java/com/axialeaa/simplehollowlogs/SimpleHollowLogs.java
+++ src/main/java/com/axialeaa/simplehollowlogs/SimpleHollowLogs.java
@@ -4,17 +4,8 @@ import com.axialeaa.simplehollowlogs.block.ModBlocks;
 import com.axialeaa.simplehollowlogs.block.ModItemGroups;
 import com.axialeaa.simplehollowlogs.util.ModBlockBehaviours;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static com.axialeaa.simplehollowlogs.world.HollowLogPlacedFeatures.*;
-import static net.minecraft.world.biome.BiomeKeys.*;
 
 public class SimpleHollowLogs implements ModInitializer {
 	public static final String MOD_ID = "simple-hollow-logs";
@@ -23,26 +14,14 @@ public class SimpleHollowLogs implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
 		ModItemGroups.registerItemGroups();
+
 		ModBlockBehaviours.registerFlammables();
+		ModBlockBehaviours.placeInBiome();
+
 		ModBlocks.onClassInitialized();
 
-		addLogFeature(FALLEN_BIRCH_FOREST_LOG, 		BIRCH_FOREST, OLD_GROWTH_BIRCH_FOREST);
-		addLogFeature(FALLEN_CHERRY_GROVE_LOG, 		CHERRY_GROVE);
-		addLogFeature(FALLEN_DARK_FOREST_LOG, 		DARK_FOREST);
-		addLogFeature(FALLEN_FOREST_LOG, 			FOREST, FLOWER_FOREST);
-		addLogFeature(FALLEN_JUNGLE_LOG, 			JUNGLE, BAMBOO_JUNGLE);
-		addLogFeature(FALLEN_SPARSE_JUNGLE_LOG, 	SPARSE_JUNGLE);
-		addLogFeature(FALLEN_TAIGA_LOG, 			TAIGA, SNOWY_TAIGA, GROVE, OLD_GROWTH_PINE_TAIGA, OLD_GROWTH_SPRUCE_TAIGA);
-		addLogFeature(FALLEN_WINDSWEPT_FOREST_LOG, 	WINDSWEPT_FOREST);
-
 		LOGGER.info(MOD_NAME + " initialized. That's all you need to know. This is a hollow logger.");
-	}
-
-	@SafeVarargs
-	public static void addLogFeature(RegistryKey<PlacedFeature> logFeature, RegistryKey<Biome>... biomeKeys) {
-		BiomeModifications.addFeature(BiomeSelectors.includeByKey(biomeKeys), GenerationStep.Feature.VEGETAL_DECORATION, logFeature);
 	}
 
 }
--- src/main/java/com/axialeaa/simplehollowlogs/util/ModBlockBehaviours.java
+++ src/main/java/com/axialeaa/simplehollowlogs/util/ModBlockBehaviours.java
@@ -1,11 +1,37 @@
 package com.axialeaa.simplehollowlogs.util;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
+
+import static com.axialeaa.simplehollowlogs.world.HollowLogPlacedFeatures.*;
+import static com.axialeaa.simplehollowlogs.world.HollowLogPlacedFeatures.FALLEN_WINDSWEPT_FOREST_LOG;
+import static net.minecraft.world.biome.BiomeKeys.*;
 
 public class ModBlockBehaviours {
 
     public static void registerFlammables() {
         FlammableBlockRegistry.getDefaultInstance().add(ModTags.HOLLOW_LOGS_THAT_BURN, 5, 5);
+    }
+
+    public static void placeInBiome() {
+        createLogFeature(FALLEN_BIRCH_FOREST_LOG, 		BIRCH_FOREST, OLD_GROWTH_BIRCH_FOREST);
+        createLogFeature(FALLEN_CHERRY_GROVE_LOG, 		CHERRY_GROVE);
+        createLogFeature(FALLEN_DARK_FOREST_LOG, 		DARK_FOREST);
+        createLogFeature(FALLEN_FOREST_LOG, 			FOREST, FLOWER_FOREST);
+        createLogFeature(FALLEN_JUNGLE_LOG, 			JUNGLE, BAMBOO_JUNGLE);
+        createLogFeature(FALLEN_SPARSE_JUNGLE_LOG, 	    SPARSE_JUNGLE);
+        createLogFeature(FALLEN_TAIGA_LOG, 			    TAIGA, SNOWY_TAIGA, GROVE, OLD_GROWTH_PINE_TAIGA, OLD_GROWTH_SPRUCE_TAIGA);
+        createLogFeature(FALLEN_WINDSWEPT_FOREST_LOG, 	WINDSWEPT_FOREST);
+    }
+
+    @SafeVarargs
+    public static void createLogFeature(RegistryKey<PlacedFeature> logFeature, RegistryKey<Biome>... biomeKeys) {
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(biomeKeys), GenerationStep.Feature.VEGETAL_DECORATION, logFeature);
     }
 
 }
